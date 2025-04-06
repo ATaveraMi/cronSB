ed14cfe03ee0898df3732edac1b5b4020e0447c3
Tavera <andrestavera03@gmail.com>
2025-04-06 14:34:29 -0600

    First trading bot commit

---
+++ _noon_curve.py
@@ -0,0 +1,5 @@
+from strategies.noon_curve import run
+
+if __name__ == "__main__":
+    run()
+
---
+++ strategies/noon_curve.py
@@ -0,0 +1,30 @@
+from config import account, trading_client, search_params
+from alpaca.trading.requests import GetAssetsRequest, StopOrderRequest, OrderRequest, MarketOrderRequest, LimitOrderRequest, TakeProfitRequest, StopLossRequest, TrailingStopOrderRequest
+from alpaca.trading.enums import OrderSide, TimeInForce, OrderClass
+def run():
+    """
+    Purchase an asset with a trailing stop
+    """
+    symbol = "APPL"
+    quantity = 1
+    
+    try:
+        quantity = float(quantity) 
+    except ValueError:
+        return jsonify({
+            'error': 'Quantity must be a valid number'
+        }), 400
+    order = trading_client.submit_order(MarketOrderRequest(
+        symbol=symbol,
+        qty=quantity,
+        side=OrderSide.BUY,
+        time_in_force=TimeInForce.DAY
+    ))
+    trailing_stop = trading_client.submit_order(TrailingStopOrderRequest(
+        symbol=symbol,
+        qty=quantity,
+        side=OrderSide.SELL,
+        time_in_force=TimeInForce.DAY,
+        trail_percent=0.01
+    ))
+    return "Trailing stop order submitted"
